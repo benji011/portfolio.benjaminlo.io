ba71e46c463d87cfcd9ed3badb1a0c8dde1fbc2f
Ben <wolfnun011@gmail.com>
2020-10-09 18:42:09 +0900

    fix: tabbed resume template & dark <-> light mode images updated

--- src/components/landing/Resume/index.jsx
+++ src/components/landing/Resume/index.jsx
@@ -20,7 +20,7 @@ export const Resume = () => {
             <TabList className={(theme === "light" ? "light-tab" : "dark-tab")}>
               <Tab>Experience</Tab>
               <Tab>Education</Tab>
-              <Tab>Technologies</Tab>
+              <Tab>Skills & technologies</Tab>
             </TabList>
 
             <TabPanel>
@@ -66,38 +66,23 @@ export const Resume = () => {
               <table>
                 <tr>
                   <th>Company</th>
-                  <th>Contact</th>
-                  <th>Country</th>
-                </tr>
-                <tr>
                   <td>Alfreds Futterkiste</td>
-                  <td>Maria Anders</td>
-                  <td>Germany</td>
                 </tr>
                 <tr>
-                  <td>Centro comercial Moctezuma</td>
+                  <th>Contact</th>
+                  <td>Alfreds Futterkiste</td>
+                </tr>
+                <tr>
+                  <th>Contact</th>
                   <td>Francisco Chang</td>
-                  <td>Mexico</td>
                 </tr>
                 <tr>
-                  <td>Ernst Handel</td>
-                  <td>Roland Mendel</td>
-                  <td>Austria</td>
+                  <th>Contact</th>
+                  <td>Francisco Chang</td>
                 </tr>
                 <tr>
-                  <td>Island Trading</td>
-                  <td>Helen Bennett</td>
-                  <td>UK</td>
-                </tr>
-                <tr>
-                  <td>Laughing Bacchus Winecellars</td>
-                  <td>Yoshi Tannamuri</td>
-                  <td>Canada</td>
-                </tr>
-                <tr>
-                  <td>Magazzini Alimentari Riuniti</td>
-                  <td>Giovanni Rovelli</td>
-                  <td>Italy</td>
+                  <th>Contact</th>
+                  <td>Francisco Chang</td>
                 </tr>
               </table>
             </TabPanel>
