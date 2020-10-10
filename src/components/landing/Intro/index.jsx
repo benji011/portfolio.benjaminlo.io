453171c642cceb8de399f88480ffd9c2edfd1d40
Ben <wolfnun011@gmail.com>
2020-10-10 11:44:42 +0900

    fix: Render contents depending on theme

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -4,7 +4,6 @@ import { ThemeContext } from 'providers/ThemeProvider';
 import { Header } from 'components/theme';
 import { Container, Button } from 'components/common';
 import dev_light from 'assets/illustrations/dev_light.svg';
-import dev_dark from 'assets/illustrations/dev_dark.svg';
 import { Wrapper, DarkWrapper, IntroWrapper, Details, Thumbnail } from './styles';
 import { useTranslation } from 'react-i18next';
 
@@ -12,41 +11,35 @@ export const Intro = () => {
   const { theme } = useContext(ThemeContext);
   const { t, i18n } = useTranslation();
 
+  // Actual page contents.
+  const contents = (
+    <IntroWrapper as={Container}>
+      <Details theme={theme}>
+        <h1>{t('hi')}</h1>
+        <h4>{t('introduction')}</h4>
+        <Button className={(theme === "light") ? "dark-btn" : "light-btn"} as={AnchorLink} href="#contact">
+          {t('contact me')}
+        </Button>
+      </Details>
+      <Thumbnail>
+        <img src={dev_light} alt={t('introduction')} />
+      </Thumbnail>
+    </IntroWrapper>
+  )
+
+  // Return page layout depending on theme set
   if (theme === "light") {
     return (
       <DarkWrapper>
         <Header />
-        <IntroWrapper as={Container}>
-          <Details theme={theme}>
-            {console.log(t('hi'))}
-            <h1>{t('hi')}</h1>
-            <h4>{t('introduction')}</h4>
-            <Button className="dark-btn" as={AnchorLink} href="#contact">
-              {t('contact me')}
-            </Button>
-          </Details>
-          <Thumbnail>
-            <img src={dev_light} alt="I’m Ben and I’m a full stack engineer!" />
-          </Thumbnail>
-        </IntroWrapper>
+        {contents}
       </DarkWrapper>
     );
   } else {
     return (
       <Wrapper>
         <Header />
-        <IntroWrapper as={Container}>
-          <Details theme={theme}>
-            <h1>{t('hi')}</h1>
-            <h4>{t('introduction')}</h4>
-            <Button className="light-btn" as={AnchorLink} href="#contact">
-              {t('contact me')}
-            </Button>
-          </Details>
-          <Thumbnail>
-            <img src={dev_dark} alt="I’m Ben and I’m a full stack engineer!" />
-          </Thumbnail>
-        </IntroWrapper>
+        {contents}
       </Wrapper>
     );
   }
