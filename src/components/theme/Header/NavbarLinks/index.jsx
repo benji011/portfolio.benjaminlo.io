a4a0cc84b32172af54a7357edb6e3b5a238b2513
Martin Kruger <martin@martinkruger.com>
2020-10-13 09:41:26 +0200

    Added provider, added Toggle Language to Header

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,40 +1,30 @@
-import React, { useContext } from "react";
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import { ThemeContext } from "providers/ThemeProvider";
-import ToggleTheme from "components/theme/Header/ToggleTheme";
-import { Wrapper } from "./styles";
+import React, { useContext } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { ThemeContext } from 'providers/ThemeProvider';
+import ToggleTheme from 'components/theme/Header/ToggleTheme';
+import ToggleLanguage from 'components/theme/Header/ToggleLanguage';
+import { Wrapper } from './styles';
 
-import header from "~/data/theme/header.json";
+import header from '~/data/theme/header.json';
 
 const NavbarLinks = ({ desktop }) => {
   const { theme } = useContext(ThemeContext);
 
   return (
     <Wrapper desktop={desktop} theme={theme}>
-      <AnchorLink
-        className={theme === "light" ? "dark-link" : "light-link"}
-        href="#about"
-      >
+      <AnchorLink className={theme === 'light' ? 'dark-link' : 'light-link'} href="#about">
         {header.about}
       </AnchorLink>
-      <AnchorLink
-        className={theme === "light" ? "dark-link" : "light-link"}
-        href="#projects"
-      >
+      <AnchorLink className={theme === 'light' ? 'dark-link' : 'light-link'} href="#projects">
         {header.projects}
       </AnchorLink>
-      <AnchorLink
-        className={theme === "light" ? "dark-link" : "light-link"}
-        href="#resume"
-      >
+      <AnchorLink className={theme === 'light' ? 'dark-link' : 'light-link'} href="#resume">
         {header.resume}
       </AnchorLink>
-      <AnchorLink
-        className={theme === "light" ? "dark-link" : "light-link"}
-        href="#contact"
-      >
+      <AnchorLink className={theme === 'light' ? 'dark-link' : 'light-link'} href="#contact">
         {header.contact}
       </AnchorLink>
+      <ToggleLanguage />
       <ToggleTheme />
     </Wrapper>
   );
--- src/components/theme/Header/ToggleLanguage/index.jsx
+++ src/components/theme/Header/ToggleLanguage/index.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "providers/ThemeProvider";
-import translateDark from "assets/icons/translation_dark.svg";
-import translateLight from "assets/icons/translation_light.svg";
-import { Wrapper } from "./styles";
-import { useTranslation } from "react-i18next";
+import React, { useContext } from 'react';
+import { ThemeContext } from 'providers/ThemeProvider';
+import translateDark from 'assets/icons/translation_dark.svg';
+import translateLight from 'assets/icons/translation_light.svg';
+import { useTranslation } from 'react-i18next';
+import { Wrapper } from './styles';
 
 const ToggleLanguage = () => {
   const { theme } = useContext(ThemeContext);
@@ -13,17 +13,16 @@ const ToggleLanguage = () => {
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
-  let i18nCheck =
-    i18n.language.toLowerCase().includes("en") ||
-    i18n.language.toLowerCase().includes("en-");
+
+  const i18nCheck = i18n.language.toLowerCase().includes('en') || i18n.language.toLowerCase().includes('en-');
 
   return (
     <Wrapper type="button">
       <img
         className="icon inverted-icon"
-        src={theme === "light" ? translateLight : translateDark}
+        src={theme === 'light' ? translateLight : translateDark}
         alt={theme}
-        onClick={() => changeLanguage(i18nCheck ? "ja" : "en")}
+        onClick={() => changeLanguage(i18nCheck ? 'ja' : 'en')}
       />
     </Wrapper>
   );
