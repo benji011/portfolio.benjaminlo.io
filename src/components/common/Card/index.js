e846de9e26e36f55b0e521c66beb0a0aba40e9fc
Ben <wolfnun011@gmail.com>
2020-10-14 15:46:10 +0900

    update: query to get languages for each repo

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -4,4 +4,11 @@ export const Card = styled.div`
   padding: 1rem;
   background: ${({ theme }) => (theme === "light" ? "#fff" : "#181717")};
   height: 100%;
+  outline: none;
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
