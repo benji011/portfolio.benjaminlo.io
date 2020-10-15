87b923b5d2f416e5676c78ffdd472bb28c20741c
ankitbeniwal <ab.ankitbeniwal@gmail.com>
2020-10-16 01:58:14 +0530

    Added scroll Position provider

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,15 +1,21 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import useDarkMode from "hooks/useDarkMode";
 
 export const ThemeContext = createContext("light");
 
 export default ({ children }) => {
   const [theme, toggleTheme] = useDarkMode();
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  document.addEventListener('scroll', () => {
+    setScrollPosition(window.scrollY)
+  })
 
   return (
     <ThemeContext.Provider
       value={{
         theme,
+        scrollPosition,
         toggleTheme,
       }}
     >
