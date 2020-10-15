636392c14fadca2f8ef60e47c38c39e6940e68c2
ankitbeniwal <ab.ankitbeniwal@gmail.com>
2020-10-16 02:19:09 +0530

    Added Inverted Sun Icon

--- src/components/theme/Header/ToggleTheme/index.jsx
+++ src/components/theme/Header/ToggleTheme/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "providers/ThemeProvider";
 import sunIcon from "assets/icons/sun.svg";
+import sunIcon-inverted from "assets/icons/sun-inverted.svg";
 import moonIcon from "assets/icons/moon.svg";
 import { Wrapper } from "./styles";
 
@@ -11,7 +12,7 @@ const ToggleTheme = () => {
     <Wrapper type="button" onClick={toggleTheme}>
       <img
         className="inverted-icon"
-        src={theme === "light" ? moonIcon : sunIcon} //Todo: Add Light Icon for scrolled navbar
+        src={theme === "light" ? moonIcon : scrollPosition > 300 ? sunIcon-inverted : sunIcon}
         alt={theme}
       />
     </Wrapper>
