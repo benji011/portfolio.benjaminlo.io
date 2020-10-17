e846f39e80b31063d5a1dd2dfe2801a572492b38
Ben <wolfnun011@gmail.com>
2020-10-17 13:00:56 +0900

    fix: removed 'document.addEventListener' from ThemeContext

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,34 +1,15 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 import useDarkMode from 'hooks/useDarkMode';
 
 export const ThemeContext = createContext('light');
 
 export default ({ children }) => {
   const [theme, toggleTheme] = useDarkMode();
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  let scrolling = false;
-
-  document.addEventListener(
-    'scroll',
-    () => {
-      scrolling = true;
-    },
-    { passive: true }
-  );
-
-  setInterval(() => {
-    if (scrolling) {
-      scrolling = false;
-      setScrollPosition(window.scrollY);
-    }
-  }, 500);
 
   return (
     <ThemeContext.Provider
       value={{
         theme,
-        scrollPosition,
         toggleTheme,
       }}
     >
