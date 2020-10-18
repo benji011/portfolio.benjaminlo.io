147bfe85ee28f1bae77a8a73f6b3a03952be85d9
Ben <wolfnun011@gmail.com>
2020-10-18 17:09:57 +0900

    fix: temporarily add 404 image then redirect to home page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
+import { I18nextProvider, withTranslation } from 'react-i18next';
+import i18n from '~/i18n/index';
+import error from 'assets/illustrations/404_page_not_found_.svg';
+import { Image } from './styles.js';
 
-export default () => (
-  <Layout>
-    <SEO title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+function IndexNotFound() {
+  if (typeof window !== 'undefined') {
+    window.location = '/';
+  }
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Layout>
+        <SEO title="404: Not found" location="/404" />
+        <Image>
+          <img src={error} alt="404!" />
+        </Image>
+      </Layout>
+    </I18nextProvider>
+  );
+}
+
+export default withTranslation()(IndexNotFound);
